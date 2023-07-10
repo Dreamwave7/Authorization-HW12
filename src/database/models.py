6c22e8fd5b54a8c448ab5aec84cb4506838b5423
Dreamwave7 <dima190179@gmail.com>
2023-07-10 22:18:30 +0300

    g

--- src/database/models.py
+++ src/database/models.py
@@ -34,4 +34,15 @@ class Tag(Base):
         )
     id = Column(Integer(), primary_key=True)
     name = Column(String(30), nullable=False)
-    user_id = Column("user_id", ForeignKey("users.id", ondelete="CASCADE"))
+    user_id = Column("user_id", ForeignKey("users.id", ondelete="CASCADE"), default=None)
+    user = relationship("User", backref="tags")
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer(), primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250),nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column("created_at",DateTime(), default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
