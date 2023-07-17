b6c747c7e0c2c338b6546c664ec845e772e48c53
Dreamwave7 <dima190179@gmail.com>
2023-07-17 20:33:27 +0300

    а

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
-from src.routes import contacts
+from src.routes import auth, notes, tags
 
 app = FastAPI()
 
 
-app.include_router(contacts.router, prefix="/api")
+app.include_router(auth.router, prefix="/api")
+app.include_router(tags.router, prefix = "/api")
+app.include_router(notes.router, prefix="/api")
 
 @app.get("/")
 def read_root():
